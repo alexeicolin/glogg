alexeicolin/glogg

commit 7a5d77672e884ed64402da2051b0f8daa2bbc5ec

Restrict fonts to mono-space

abstractlogview.cpp
@@ -109,13 +109,14 @@ inline void LineDrawer::draw( QPainter& painter,
 {
     QFontMetrics fm = painter.fontMetrics();
     const int fontHeight = fm.height();
+    const int fontWidth = fm.maxWidth();
     const int fontAscent = fm.ascent();
 
     foreach ( Chunk chunk, list ) {
         // Draw each chunk
         // LOG(logDEBUG) << "Chunk: " << chunk.start() << " " << chunk.length();
         QString cutline = line.mid( chunk.start(), chunk.length() );
-        const int chunk_width = fm.width( cutline );
+        const int chunk_width = cutline.length() * fontWidth;
         painter.fillRect( xPos, yPos, chunk_width,
                 fontHeight, chunk.backColor() );
         painter.setPen( chunk.foreColor() );
@@ -203,7 +204,6 @@ AbstractLogView::AbstractLogView(const AbstractLogData* newLogData,
     overview_ = NULL;
 
     // Fonts
-    useFixedFont_ = false;
     charWidth_ = 1;
     charHeight_ = 1;
 
@@ -563,7 +563,7 @@ void AbstractLogView::paintEvent( QPaintEvent* paintEvent )
     {
         // Repaint the viewport
         QPainter painter( viewport() );
-        const int fontHeight = painter.fontMetrics().height();
+        const int fontHeight = charHeight_;
         const int fontAscent = painter.fontMetrics().ascent();
         const int nbCols = getNbVisibleCols();
         const QPalette& palette = viewport()->palette();
@@ -885,19 +885,10 @@ void AbstractLogView::updateData()
 
 void AbstractLogView::updateDisplaySize()
 {
+    // Font is assumed to be mono-space (is restricted by options dialog)
     QFontMetrics fm = fontMetrics();
-
     charHeight_ = fm.height();
-    // Determine if the font is monospace (to enable various optimisations)
-    useFixedFont_ = true;
-    charWidth_ = fm.width('i');
-    for ( int i = 0x20; i < 0x7F; i++ )
-        if ( fm.width( QLatin1Char( i ) ) != charWidth_ ) {
-            useFixedFont_ = false;
-            break;
-        }
-    // FIXME: seems to be wrong on Qt/Mac
-    LOG(logDEBUG) << "useFixedFont_=" << useFixedFont_;
+    charWidth_ = fm.maxWidth();
 
     // Calculate the index of the last line shown
     lastLine = qMin( logData->getNbLine(), firstLine + getNbVisibleLines() );
@@ -967,8 +958,7 @@ int AbstractLogView::getNbVisibleCols() const
 // Converts the mouse x, y coordinates to the line number in the file
 int AbstractLogView::convertCoordToLine(int yPos) const
 {
-    QFontMetrics fm = fontMetrics();
-    int line = firstLine + yPos/fm.height();
+    int line = firstLine + yPos / charHeight_;
 
     return line;
 }
@@ -983,21 +973,11 @@ QPoint AbstractLogView::convertCoordToFilePos( const QPoint& pos ) const
     if ( line < 0 )
         line = 0;
 
-    int column = 0;
-    // Determine column in screen space
-    QFontMetrics fm = fontMetrics();
+    // Determine column in screen space and convert it to file space
+    int column = firstCol + ( pos.x() - leftMarginPx_ ) / charWidth_;
+
     QString this_line = logData->getExpandedLineString( line );
     const int length = this_line.length();
-    for ( ; column < length; column++ ) {
-        if ( ( fm.width( this_line.mid(firstCol, column) )
-                    + leftMarginPx_ ) > pos.x() ) {
-            column--;
-            break;
-        }
-    }
-
-    // Now convert it to file space
-    column += firstCol;
 
     if ( column >= length )
         column = length - 1;

abstractlogview.h
@@ -264,8 +264,7 @@ class AbstractLogView : public QAbstractScrollArea
     int firstCol;
 
     // Text handling
-    bool useFixedFont_;
-    int charWidth_;             // Must only be used if useFixedFont_ == true
+    int charWidth_;
     int charHeight_;
 
     // Popup menu

optionsdialog.cpp
@@ -50,7 +50,11 @@ void OptionsDialog::setupFontList()
 {
     QFontDatabase database;
 
-    fontFamilyBox->addItems(database.families());
+    // We only show the fixed fonts
+    foreach ( const QString &str, database.families() ) {
+         if ( database.isFixedPitch( str ) )
+             fontFamilyBox->addItem( str );
+     }
 }
 
 // Populate the regexp ComboBoxes

commit ce75e320a63217269be2c8cd0a87f1bce78e4ea9

Fix for chunk width calculation: char bearings
This fixes a bug introduced in 7a5d77672e884ed64402da2051b0f8daa2bbc5ec.
Chunks for highlights or selection were drawn off by one pixel
because the chunk width calculation did not take into account character
bearings. This commit reverts the chunk width calculation to what it
was before.

abstractlogview.cpp
@@ -109,14 +109,17 @@ inline void LineDrawer::draw( QPainter& painter,
 {
     QFontMetrics fm = painter.fontMetrics();
     const int fontHeight = fm.height();
-    const int fontWidth = fm.maxWidth();
     const int fontAscent = fm.ascent();
 
     foreach ( Chunk chunk, list ) {
         // Draw each chunk
         // LOG(logDEBUG) << "Chunk: " << chunk.start() << " " << chunk.length();
         QString cutline = line.mid( chunk.start(), chunk.length() );
-        const int chunk_width = cutline.length() * fontWidth;
+        // Note that fm.width is not the same as len * maxWidth due to
+        // bearings.  We could use the latter and take bearings into account,
+        // but perhaps the font metics already makes width() efficient for
+        // fixed width fonts.
+        const int chunk_width = fm.width( cutline );
         painter.fillRect( xPos, yPos, chunk_width,
                 fontHeight, chunk.backColor() );
         painter.setPen( chunk.foreColor() );